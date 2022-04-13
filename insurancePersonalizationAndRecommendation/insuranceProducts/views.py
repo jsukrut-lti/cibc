0a95781f2beabf55d6e8594c5d849dec0e917e81
10700313 <nilesh1.patil@lntinfotech.com>
2022-04-13 11:49:54 +0530

    changes for API messages

--- insurancePersonalizationAndRecommendation/insuranceProducts/views.py
+++ insurancePersonalizationAndRecommendation/insuranceProducts/views.py
@@ -269,21 +269,61 @@ class InsuranceDiscussionAPI(APIView):
     """
         Retrieve, update or delete a InsuranceDiscussion i.
     """
-    def get(self, request):
-        insurance_discussions = InsuranceDiscussion.objects.all()
-        serializer = InsuranceDiscussionSerializers(insurance_discussions, many=True)
-        if insurance_discussions:
-            return Response({"result": "success",
-                             "data": list(serializer.data),
-                             "status": status.HTTP_200_OK})
+    def response(self, data=None, success=True, **message):
+        if data and success:
+            return Response(data={"result": "success", "data": data},
+                            status=status.HTTP_200_OK)
+        elif success:
+            return Response(data={"result": "success"},
+                            status=status.HTTP_200_OK)
         else:
-            return Response({"result": "No data found"})
+            return Response(data={"result": "failed", "message": message.get("err", "UNKNOWN ERROR")},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        if request.request.GET['id']:
+            insurance_discussions = InsuranceDiscussion.objects.filter(id=request.GET['id']).values()
+        else:
+            insurance_discussions = InsuranceDiscussion.objects.all().values()
+        if insurance_discussions:
+            self.response(data=insurance_discussions)
+        else:
+            self.response(success=False, err='No data found')
+
+
+    def post(self, request):
+        serializer = InsuranceDiscussionSerializers(data=request.data)
+        if serializer.is_valid():
+            save_rec = serializer.save()
+            self.response(data=[{'record_id': save_rec}])
+        else:
+            self.response(success=False, err=serializer.errors)
+
+
+    def put(self, request):
+        dis = InsuranceDiscussion.objects.get(id=request.GET['id'])
+        serializer = InsuranceDiscussionSerializers(instance=dis, data=request.data, partial=False)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            self.response(data=serializer.validated_data)
+        else:
+            self.response(success=False, err=serializer.errors)
+
+
+    def delete(self, request):
+        insurance_discussion = InsuranceDiscussion.objects.filter(id=request.GET['id'])
+        if insurance_discussion:
+            insurance_discussion.delete()
+            self.response()
+        else:
+            self.response(success=False, err="No data found")
+
+
 
 
 class InsuranceDiscussionCreate(APIView):
 
     def post(self, request):
-        print("----",request.data)
         serializer = InsuranceDiscussionSerializers(data=request.data)
         if serializer.is_valid():
             save_rec = serializer.save()
