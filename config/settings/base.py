997d406e3514fcc5496b69f31e748252b4c6123b
jayeshpatillnt <jayeshc.patil@lntinfotech.com>
2022-04-13 19:06:00 +0530

    add swagger

--- config/settings/base.py
+++ config/settings/base.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_yasg',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
