a56f5edb48bf82849db701b9315a8ef0418ac117
jayeshpatillnt <jayeshc.patil@lntinfotech.com>
2022-04-18 15:52:12 +0530

    testing

--- config/urls.py
+++ config/urls.py
@@ -48,5 +48,6 @@ urlpatterns = [
     path('insurance_api/',include('insurancePersonalizationAndRecommendation.insuranceProducts.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
