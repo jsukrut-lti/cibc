68700277ca9a6ea592c3800e4e00358dcafa9868
jsukrut-lti <sukrut.joshi@lntinfotech.com>
2022-06-29 17:24:40 +0530

    fix:env

--- config/settings/base.py
+++ config/settings/base.py
@@ -114,12 +114,12 @@ LOGIN_REDIRECT_URL = "/account/home/"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
--- config/settings/local.py
+++ config/settings/local.py
@@ -4,15 +4,14 @@ DEBUG = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cibc',
-        'HOST': 'localhost',
+        'NAME': 'cibcdevdb',
+        'HOST': 'cibc-dev-db.c8aqzm8dqrzg.ca-central-1.rds.amazonaws.com',
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': '8mBUEdN0F13jStclgrpr',
         'PORT': '5432',
     }
 }
-
-# INSTALLED_APPS += ['debug_toolbar',]
